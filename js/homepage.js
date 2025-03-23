73eea75e88f5b01822c1a5d41f12c262ae66611c
inesrmcosta20 <inesrmcosta20@gmail.com>
2025-03-23 22:19:01 +0000

    imagens
    alteração da imagens

--- js/homepage.js
+++ js/homepage.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const aviao = document.getElementById('aviao');
     aviao.addEventListener('animationend', function () {
       // Substitui a imagem quando a animação terminar
-      aviao.src = 'imagens/aviao-sombra.png';
+      aviao.src = 'imagens/aviãofinal-preto.png';
     });
   });
   
