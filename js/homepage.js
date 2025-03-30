e0fcb40a73d869c0c5aa68e184609e1f454efe9a
inesrmcosta20 <inesrmcosta20@gmail.com>
2025-03-30 04:22:50 +0100

    html e js
    ajuste posição das peças. codigo js comentado sobre opacidade - não funciona

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const aviao = document.getElementById("aviao");
 
+
   aviao.addEventListener("animationend", function () {
-      aviao.src = "imagens/aviao_sombra.png";
+    aviao.src = "imagens/aviao_sombra.png";
+  
   });
-});
+
+  /*
+
+*/
+}); 
