170a80740c0095cea8a8c525161f8f510a8d25ac
inesmqpereira <inesquinapereira@gmail.com>
2025-03-31 19:37:39 +0100

    update

--- js/rosa.js
+++ js/rosa.js
@@ -1,3 +1,19 @@
+//Intro Jogo Rosa
+document.addEventListener("DOMContentLoaded", function () {
+    let introRosa = document.getElementById("intro-rosa");
+  
+    if (introRosa) {
+      introVideo.addEventListener("ended", function () {
+        introRosa.style.opacity = "0"; // Reduz gradualmente a opacidade
+        setTimeout(() => {
+          introRosa.style.display = "none"; // Remove completamente após a animação
+        }, 1000);
+      });
+    }
+  });
+  
+
+// Mover objetos
 document.addEventListener("DOMContentLoaded", function () {
   function makeDraggable(element, scaleInside) {
       let isDragging = false;
