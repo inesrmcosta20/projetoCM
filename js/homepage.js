8be2f48e60deb4aeab92f36c72b02b70a9935a27
inesmqpereira <inesquinapereira@gmail.com>
2025-04-03 20:03:59 +0100

    updates

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,4 @@
+//Substituição no final da animação
 document.addEventListener("DOMContentLoaded", function () {
   const aviao = document.getElementById("aviao");
   const missaoRosa = document.querySelector(".missao-rosa");
@@ -18,3 +19,32 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 */
 
+//Controlo do som
+document.addEventListener("DOMContentLoaded", function () {
+  const somOn = document.getElementById("som-on");
+  const somOff = document.getElementById("som-off");
+  const audio = document.getElementById("background-music");
+
+  // Inicializa os ícones de som corretamente
+  somOn.style.display = "none";
+  somOff.style.display = "block";
+
+  // Verifica se o áudio está em reprodução e ajusta os ícones
+  function toggleAudio() {
+    if (audio.paused) {
+      audio.play();
+      somOff.style.display = "none";
+      somOn.style.display = "block";
+    } else {
+      audio.pause();
+      somOff.style.display = "block";
+      somOn.style.display = "none";
+    }
+  }
+
+  // Quando o ícone de som desligado for clicado
+  somOff.addEventListener("click", toggleAudio);
+
+  // Quando o ícone de som ligado for clicado
+  somOn.addEventListener("click", toggleAudio);
+});
