5ceb08d995d5d872344bef4e2bee612a9fc6817a
inesrmcosta20 <inesrmcosta20@gmail.com>
2025-04-16 19:22:14 +0100

    planeta candeeiros
    atualizar

--- js/candeeiros.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const planeta = document.getElementById('planeta');
-  const lamps = [
-      document.getElementById('luz1'),
-      document.getElementById('luz2'),
-      document.getElementById('luz3')
-  ];
- 
