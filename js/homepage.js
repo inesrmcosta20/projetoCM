34a415fc8e5c84a7e152b7c0313621c88dfc5f9c
inesrmcosta20 <inesrmcosta20@gmail.com>
2025-04-29 20:16:51 +0100

    animaçao
    animação avião

--- js/homepage.js
+++ js/homepage.js
@@ -51,18 +51,29 @@ document.addEventListener("DOMContentLoaded", function () {
   // Executa ao carregar a página
   startAudio();
 
-  // Quando a animação do avião termina
-  aviao.addEventListener("animationend", function () {
-    aviao.src = "imagens/homepage-cenário/avião-sombra.png";
+ 
+  
+// Quando a animação do avião termina
+aviao.addEventListener("animationend", function() {
+  // Esconde o avião animado
+  aviao.style.display = "none";
+  
+  // Mostra o avião estático (sombra)
+  aviao2.style.display = "block";
+  
+  // Remove a animação para evitar que ocorra novamente
+  aviao.style.animation = "none";
 
-    setTimeout(() => {
+  setTimeout(() => {
       pecasCenario.style.opacity = "1";
       pecasCenario.classList.add("active");
-    }, 300);
-  });
+  }, 300);
+});
+
+
 
   // Mostrar ou esconder peças do avião com base no localStorage
-  const imagemIdMostrar = localStorage.getItem('imagemParaMostrar');
+  const imagemIdMostrar =getItem('imagemParaMostrar');
   const imagemIdEsconder = localStorage.getItem('imagemParaEsconder');
 
   if (imagemIdMostrar) {
